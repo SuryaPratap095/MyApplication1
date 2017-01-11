44c6c72e8e5c566a1821e2ee6e1c0b257f6fe74a
Surya Pratap Singh <keithgeorge095@gmail.com>
2017-01-11 20:33:39 +0530

    TextView commit

--- app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TextView textView=(TextView) findViewById(R.id.Hellotxt);
         textView.setAllCaps(true);
     }
