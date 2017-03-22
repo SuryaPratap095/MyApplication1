0bc4aead3505e16f1af7bb40296f102ca0d8464e
Surya Pratap Singh <keithgeorge095@gmail.com>
2017-03-22 14:06:06 +0200

    Shortcuts

--- app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         TextView textView=(TextView) findViewById(R.id.HelloTxt);
         textView.setAllCaps(true);TextView textView1=(TextView) findViewById(R.id.HelloTxt);
         textView1.setAllCaps(true);
