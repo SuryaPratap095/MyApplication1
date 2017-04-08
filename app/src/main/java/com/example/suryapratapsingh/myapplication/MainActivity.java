0fce0764a9a44e944c2e29618ad55279527a4425
Surya Pratap Singh <keithgeorge095@gmail.com>
2017-04-08 21:44:44 +0200

    Shortcuts

--- app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
