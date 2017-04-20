03518af658c0534d11d93785830859bd704a26c7
Surya Pratap Singh <keithgeorge095@gmail.com>
2017-04-20 15:14:14 +0200

    Shortcuts

--- app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suryapratapsingh/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ package com.example.suryapratapsingh.myapplication;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView1;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
